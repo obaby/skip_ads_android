71ba3e1da8010ba101a199e6935942a4fb105f81
obaby <obaby.lh@gmail.com>
2021-09-18 14:49:41 +0800

    init

---
+++ app/src/main/java/cn/org/obaby/adsskiper/BabyAccessibilityService.java
@@ -0,0 +1,16 @@
+package cn.org.obaby.adsskiper;
+
+import android.accessibilityservice.AccessibilityService;
+import android.view.accessibility.AccessibilityEvent;
+
+public class BabyAccessibilityService extends AccessibilityService {
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+
+    }
+
+    @Override
+    public void onInterrupt() {
+
+    }
+}
