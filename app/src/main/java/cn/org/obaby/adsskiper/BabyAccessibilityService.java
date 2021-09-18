448c8d61590840a92162795c4fbce21d0f1f5626
obaby <obaby.lh@gmail.com>
2021-09-18 16:46:45 +0800

    更新服务

--- app/src/main/java/cn/org/obaby/adsskiper/BabyAccessibilityService.java
+++ app/src/main/java/cn/org/obaby/adsskiper/BabyAccessibilityService.java
@@ -1,11 +1,16 @@
 package cn.org.obaby.adsskiper;
 
 import android.accessibilityservice.AccessibilityService;
+import android.annotation.SuppressLint;
+import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
 
 public class BabyAccessibilityService extends AccessibilityService {
+    final private String TAG = "BabyAccessibilityService";
+    @SuppressLint("LongLogTag")
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
+        Log.i(TAG, "onAccessibilityEvent: " + event.toString());
 
     }
 
